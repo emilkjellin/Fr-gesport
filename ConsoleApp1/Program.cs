64810e1497c1b2359ea849609b1dbf88fed1b960
Emil <emilv.kjellin@gmail.com>
2023-09-21 10:06:00 +0200

    suii

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,82 +1,85 @@
 ﻿
-Console.WriteLine("Hej, vad är det gulligaste djuret i världen");
-Console.WriteLine("Capybara, Wilmer eller smurfar");
-string name = "sum-"+ "Emil";  
+Console.WriteLine("Hey, what is the cutest animal on the planet?");
+Console.WriteLine("Capybara{z}, Wilmer{x} or smurfar{c}");
+string name = Console.ReadLine();  
 name = name.ToLower();
-int sum = 24 + 8;
-if(name == "capybara")
+int points = 0;
+if(name == "z")
 {
+points++;
 Console.WriteLine("Got damn right!");
-Console.WriteLine("Your score = {sum}!");
-Console.WriteLine("vad är det spanska namnet för kyckling");
-Console.WriteLine("pollo , poulet eller ዶሮ ");
+Console.WriteLine($"You have {points} point");
+Console.WriteLine("What is the spannish word for chicken?");
+Console.WriteLine("pollo{z} , poulet{x} or ዶሮ ");
 name = Console.ReadLine();
 name = name.ToLower();
-if(name == "pollo")
+if(name == "z")
 {
-Console.WriteLine("Rätt igen!");
-Console.WriteLine("Hur många gympass kör Liam per vecka");
-Console.WriteLine("6 ,3 eller 5");
+points++;
+Console.WriteLine("Right again!");
+Console.WriteLine($"You have {points} points");
+Console.WriteLine("Which company is dominating in F1 racing");
+Console.WriteLine("Redbull{y}, Ferarri{x}, lamborghini{c} ");
 name = Console.ReadLine();
 name = name.ToLower();
-if(name == "6")
+if(name == "y")
 {
-Console.WriteLine("Du är på en Streak av 3!");
-Console.WriteLine("Bra jobbat!");
+Console.WriteLine("You are on a streak of 2 right answers!");
+Console.WriteLine("Keep it up!");
 
 
 }
-else if(name == "3")
+else if(name == "x")
 {
-Console.WriteLine("FEL, DU VAR SÅ NÄRA");
+Console.WriteLine("wrong, but ferrari do race in F1");
 
 }
-else if(name == "5")
+else if(name == "c")
 {
-Console.WriteLine("HUR LYCKAS DU FÅ FEL PÅ SISTA FRÅGAN");
+Console.WriteLine("How do you choose the only company that is not competing in F1:(");
 
 }
 else
 {
-Console.WriteLine("bra försök buddy");
+Console.WriteLine("good try budddy");
 
 }
 
 }
-else if(name == "poulet")
+else if(name == "x")
 {
-Console.WriteLine("FEL!");
+Console.WriteLine("Wrong!");
 
 }
 else if(name == "ዶሮ")
 {
-Console.WriteLine("Hur i helsike lyckas du skriva detta! DU VINNER HELT OCH HÅLLET");
+Console.WriteLine("YOU WIN, YOU ARE CRAZY I DID NOT EXPECT SOMEONE TO GET THIS BUT FOR YOU WHO DID I AM GREATFUL THAT YOU COMPLETED MY GAME IN THE BEST WAY POSSIBLE!");
 
 
 
 }
 else
 {
-Console.WriteLine("SLUTA FÖRSÖKA HITTA BUGGAR!");
+Console.WriteLine("STOP TRYING TO ESCAPE THE QUESTION!");
 
 
 }
 }
-else if(name == "wilmer")
+else if(name == "x")
 {
-Console.WriteLine("Jag fattar vad du menar, men det är fel");
+Console.WriteLine("SO SO So SO SO WRONG!");
 
 }
-else if (name == "smurfar")
+else if (name == "c")
 {
-Console.WriteLine("UHUH,FEL!");
+Console.WriteLine("UHUH,WRONG!");
 
 
 }
 
 else
 {
-Console.WriteLine("det du precis har sagt är helt orimligt och du borde få fängelsetid");
+Console.WriteLine("bad boy!!! you are crazy!!");
 
 
 
